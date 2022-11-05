0c7443a82ffd5e091b50b2f3ab178a1b8ecbc585
ayomisco <fayomide324@gmail.com>
2022-11-05 13:13:11 -0400

    Updated task 2

--- api/urls.py
+++ api/urls.py
@@ -3,6 +3,6 @@ from . import views
 
 urlpatterns = [
     path('', views.getData),
-    path('calculate', views.addData)
+    path('calculate/', views.addData)
 
 ]
--- api/views.py
+++ api/views.py
@@ -1,5 +1,8 @@
+from http.client import responses
 from rest_framework.response import Response
+from rest_framework import status
 from rest_framework.decorators import api_view
+
 
 @api_view(['GET'])
 def getData(request):
