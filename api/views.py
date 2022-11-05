aab36ee522f76fd88e5b7fcea11929fa93deadf2
ayomisco <fayomide324@gmail.com>
2022-11-05 13:04:03 -0400

    Updated task 2

--- api/views.py
+++ api/views.py
@@ -5,3 +5,33 @@ from rest_framework.decorators import api_view
 def getData(request):
     person = {'slackUsername': 'ayomisco', 'backend':True, 'age': 23, 'bio':"I'm a tech lover."}
     return Response(person)
+
+
+@api_view(['POST'])
+def addData(request):
+        # serializer = ArithemeticSerializer(data=request.data)
+        # if serializer.is_valid():
+        #     # serializer.save()
+        # return Response(serializer.data)
+        x = int(request.data["x"])
+        y = int(request.data["y"])
+        operation_type = request.data["operation_type"]
+
+        if operation_type in ['addition','add','+','subtraction','minus','subtract','-','multiply','x','*','multiplication']:
+            if operation_type == 'addition' or operation_type == 'add' or operation_type == '+': 
+                result = int(x) + int(y)
+            if operation_type == 'subtraction' or operation_type == 'minus' or operation_type == 'subtract' or operation_type == 'subtract': 
+                result = int(x) - int(y)
+            if operation_type == 'multiply' or operation_type == 'x' or operation_type == 'multiplication' or operation_type == '*': 
+                result = int(x) * int(y)
+
+            user_response = {
+                'slackUsername': 'ayomisco',
+                'result': result,
+                'operation_type':operation_type,
+            }
+
+            return Response(user_response, status.HTTP_200_OK)
+        else:
+            content= {"Invalid operation. I don't catch what '{}' means.".format(operation_type)}
+            return Response(content, status=status.HTTP_400_BAD_REQUEST)
